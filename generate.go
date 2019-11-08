510588280f58f246ed76c3eeceb72b18b401feb6
Matthew Hughes <mhughes@rtautomation.com>
2019-11-07 18:08:12 -0600

    Since this is based on the original Go example, I'm including the Go LICENSE File

--- generate.go
+++ generate.go
@@ -1,5 +1,16 @@
-// generate-tls-cert generates root, leaf, and client TLS certificates.
 package main
+
+// Copyright 2009 The Go Authors. All rights reserved.
+
+// Use of this source code is governed by a BSD-style
+
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+// Generate a self-signed X.509 certificate for a TLS server. Outputs to
+
+// 'cert.pem' and 'key.pem' and will overwrite existing files.
 
 import (
 	"crypto/ecdsa"
